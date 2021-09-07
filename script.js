597886d8f60577ffdc2a87623be215b6ef32add3
Maja Berendsen <maja498h@stud.kea.dk>
2021-09-07 14:42:13 +0200

    layout mobile view and JS

--- script.js
+++ script.js
@@ -2,7 +2,50 @@
 
 // api key: 61364b5343cedb6d1f97ed5f
 
-const url = "https://dogbreeds-dbcd.restdb.io/rest/dogbreeds?max=20";
+// const url = "https://dogbreeds-dbcd.restdb.io/rest/dogbreeds?max=20";
+
+// const options = {
+//   headers: {
+//     "x-apikey": "61364b5343cedb6d1f97ed5f",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw Error(response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // console.log(data);
+//     handleData(data);
+//   })
+//   .catch((e) => {
+//     console.error("An error occured:", e.message);
+//   });
+
+// function handleData(dogbreeds) {
+//   dogbreeds.forEach((dog) => {
+//     console.log(dog);
+//     // 4 populate with data
+//     document.querySelector(".retriever").addEventListener("click", changeDog);
+//     // 5 append it to the DOM
+//   });
+// }
+
+// function changeDog() {
+
+
+//   document.querySelector(".retriever");
+//   document.querySelector("h2").textContent = dog.breedname;
+//   document.querySelector(".dog-image").src = dog.img_url;
+
+//   document.querySelector("h3").textContent = dog.bio;
+
+// }
+
+const url = "https://dogbreeds-dbcd.restdb.io/rest/dogbreeds?max=15";
 
 const options = {
   headers: {
@@ -29,19 +72,23 @@ function handleData(dogbreeds) {
   dogbreeds.forEach((dog) => {
     console.log(dog);
 
-    // 1 make a template
-    // 2 grab it
-    const template = document.querySelector("template").content;
-    // 3 clone it
-    const clone = template.cloneNode(true);
-    // 4 populate with data
-    clone.querySelector("h2").textContent = dog.breedname;
-    clone.querySelector("img").src = dog.img_url;
+    document.querySelector("h2").textContent = dog.breedname;
+    document.querySelector(".dog-image").src = dog.img_url;
+    document.querySelector("h3").textContent = dog.bio;
+    document.querySelector(".furcolor").textContent = `Furcolor: ${dog.furcolor}`;
+    document.querySelector(".lifespan").textContent = `Lifespan: ${dog.lifespan} years`;
+    document.querySelector(".brachyc").textContent = `brachyc: ${dog.brachyc}`;
 
-    clone.querySelector("h3").textContent = dog.bio;
 
-    const mainEl = document.querySelector("main");
-    mainEl.appendChild(clone);
     // 5 append it to the DOM
   });
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
